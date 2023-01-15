5ba7effd6aea189f2f2563d7be4875e76a520683
Jonathan Wilke <info@jonathan-wilke.de>
2023-01-15 11:40:01 +0100

    feat: basic project setup

---
+++ modules/home/components/HomePage.tsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <div className="p-8 bg-blue-50">Hello App!</div>;
+}
---
+++ pages/index.tsx
@@ -0,0 +1,3 @@
+import HomePage from '@home/components/HomePage';
+
+export default HomePage;
