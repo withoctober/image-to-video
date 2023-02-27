5d09f054d59d25b15504eb97e56e661c516edae7
Jonathan Wilke <info@jonathan-wilke.de>
2023-02-27 10:54:53 +0100

    fix: warning for unexpected i18n config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'de'],
-    localePath: path.resolve('./public/locales'),
   },
+  localePath: path.resolve('./public/locales'),
   reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
