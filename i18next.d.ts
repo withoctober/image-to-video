3126d5938b485d36475542f8cfa541b88c721e92
Jonathan Wilke <info@jonathan-wilke.de>
2023-02-27 08:10:28 +0100

    fix(i18n): type for home translations

--- i18next.d.ts
+++ i18next.d.ts
@@ -1,10 +1,12 @@
 import 'i18next';
 import type auth from 'public/locales/en/auth.json';
 import type common from 'public/locales/en/common.json';
+import type home from 'public/locales/en/home.json';
 
 interface I18nNamespaces {
   common: typeof common;
   auth: typeof auth;
+  home: typeof home;
 }
 
 declare module 'i18next' {
