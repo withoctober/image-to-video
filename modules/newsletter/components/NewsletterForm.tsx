234548812eef5c44a68110b4d29b4c161f6048d6
Jonathan Wilke <info@jonathan-wilke.de>
2023-05-16 07:33:34 +0200

    feat: add submission for newsletter section

--- modules/newsletter/components/NewsletterForm.tsx
+++ modules/newsletter/components/NewsletterForm.tsx
@@ -1,13 +1,51 @@
 'use client';
 
 import Button from '@common/components/primitives/Button';
+import Hint from '@common/components/primitives/Hint';
 import Input from '@common/components/primitives/Input';
+import { saveEmail } from '@newsletter/server/actions';
+import { useForm } from 'react-hook-form';
+import { FiCheckCircle } from 'react-icons/fi';
 
-export default function NewsletterForm() {
-  return (
-    <div className="flex items-start">
-      <Input type="email" placeholder="Your email" />
-      <Button className="ml-4">Subscribe</Button>
-    </div>
+export default function NewsletterForm({
+  labels,
+}: {
+  labels: {
+    email: string;
+    submit: string;
+    hints: {
+      success: {
+        title: string;
+        message: string;
+      };
+    };
+  };
+}) {
+  const {
+    handleSubmit,
+    register,
+    formState: { isSubmitting, isSubmitSuccessful },
+  } = useForm<{ email: string }>();
+
+  return isSubmitSuccessful ? (
+    <Hint
+      status="success"
+      title={labels.hints.success.title}
+      message={labels.hints.success.message}
+      icon={<FiCheckCircle />}
+    />
+  ) : (
+    <form
+      onSubmit={handleSubmit(async ({ email }) => {
+        await saveEmail(email);
+      })}
+    >
+      <div className="flex items-start">
+        <Input type="email" required placeholder={labels.email} {...register('email')} />
+        <Button type="submit" className="ml-4" isLoading={isSubmitting}>
+          {labels.submit}
+        </Button>
+      </div>
+    </form>
   );
 }
--- modules/newsletter/components/NewsletterSection.tsx
+++ modules/newsletter/components/NewsletterSection.tsx
@@ -1,19 +1,32 @@
 import NewsletterForm from '@newsletter/components/NewsletterForm';
+import { getTranslations } from 'next-intl/server';
 import { FiKey } from 'react-icons/fi';
 
-export default function NewsletterSection() {
+export default async function NewsletterSection() {
+  const t = await getTranslations('newsletter');
   return (
     <section className="bg-blue-500 bg-opacity-5 py-24 dark:text-white">
       <div className="container">
         {/* Section header */}
         <div className="mb-12 text-center">
           <FiKey className="mx-auto mb-3 w-auto text-5xl text-blue-500" />
-          <h1 className="text-4xl font-bold lg:text-5xl">Get early access</h1>
-          <p className="mt-3 text-lg opacity-70">Be among the first to get access to supastarter.nextjs.</p>
+          <h1 className="text-4xl font-bold lg:text-5xl">{t('title')}</h1>
+          <p className="mt-3 text-lg opacity-70">{t('subtitle')}</p>
         </div>
 
         <div className="mx-auto max-w-lg">
-          <NewsletterForm />
+          <NewsletterForm
+            labels={{
+              email: t('email'),
+              submit: t('submit'),
+              hints: {
+                success: {
+                  title: t('hints.success.title'),
+                  message: t('hints.success.message'),
+                },
+              },
+            }}
+          />
         </div>
       </div>
     </section>
