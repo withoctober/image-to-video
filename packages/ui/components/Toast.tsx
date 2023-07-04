57e1d872c580871783536df3cad25d57ffb2a5e6
Jonathan Wilke <info@jonathan-wilke.de>
2023-07-04 08:00:24 +0200

    fix: toast styles

--- packages/ui/components/Toast.tsx
+++ packages/ui/components/Toast.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import {
   ToastCloseTrigger,
@@ -8,20 +8,21 @@ import {
   Toast as ToastPrimitive,
   ToastProvider as ToastProviderPrimitive,
   ToastTitle,
-} from '@ark-ui/react';
-import { cva } from 'class-variance-authority';
-import { PropsWithChildren } from 'react';
-import { Icon } from './Icon';
+  useToast,
+} from "@ark-ui/react";
+import { cva } from "class-variance-authority";
+import { PropsWithChildren } from "react";
+import { Icon } from "./Icon";
 
-const toastTypeClasses = cva('toastTitle', {
+const toastTypeClasses = cva("toastTitle", {
   variants: {
     type: {
-      success: ['text-emerald-500'],
-      info: ['text-primary-500'],
-      warning: ['text-amber-500'],
-      error: ['text-rose-500'],
-      loading: ['text-primary-500'],
-      custom: ['bg-zinc-700'],
+      success: ["text-emerald-500"],
+      info: ["text-primary-500"],
+      warning: ["text-amber-500"],
+      error: ["text-rose-500"],
+      loading: ["text-primary-500"],
+      custom: ["bg-zinc-700"],
     },
   },
 });
@@ -42,33 +43,45 @@ export function ToastProvider({ children }: PropsWithChildren<{}>) {
           placements.map((placement) => (
             <ToastGroup key={placement} placement={placement}>
               {(toasts) =>
-                toasts.map((toast) => (
-                  <ToastPrimitive
-                    key={toast.id}
-                    toast={toast}
-                    className={`relative w-full max-w-sm rounded-xl border bg-white p-4 py-4 pr-10 shadow-xl dark:border-zinc-700 dark:bg-zinc-900`}
-                  >
-                    <div className="flex items-center gap-3">
-                      {/* {ToastIcon ? (
-          <ToastIcon
-            className={`${toastTypeClasses({
-              type,
-              className: `text-2xl ${type === 'loading' ? 'animate-spin' : ''}`,
-            })}`}
-          />
-        ) : null} */}
+                toasts.map((toast) => {
+                  const { type } = toast.initialContext;
+                  const ToastIcon =
+                    toastTypeIcon[type as keyof typeof toastTypeIcon];
 
-                      <div>
-                        <ToastTitle className={`${toastTypeClasses({ className: 'text-base font-bold' })}`} />
-                        <ToastDescription />
-                        <ToastCloseTrigger className="absolute right-3 top-3 p-1 hover:opacity-70">
-                          <Icon.close className="h-4 w-4" />
-                          <span className="sr-only">Cancel</span>
-                        </ToastCloseTrigger>
+                  return (
+                    <ToastPrimitive
+                      key={toast.id}
+                      toast={toast}
+                      className={`relative w-full max-w-sm rounded-xl border bg-white p-4 py-4 pr-10 shadow-xl dark:border-zinc-700 dark:bg-zinc-900`}
+                    >
+                      <div className="flex items-center gap-3">
+                        {ToastIcon ? (
+                          <ToastIcon
+                            className={`${toastTypeClasses({
+                              type,
+                              className: `text-2xl ${
+                                type === "loading" ? "animate-spin" : ""
+                              }`,
+                            })}`}
+                          />
+                        ) : null}
+
+                        <div>
+                          <ToastTitle
+                            className={`${toastTypeClasses({
+                              className: "text-base font-bold",
+                            })}`}
+                          />
+                          <ToastDescription />
+                          <ToastCloseTrigger className="absolute right-3 top-3 p-1 hover:opacity-70">
+                            <Icon.close className="h-4 w-4" />
+                            <span className="sr-only">Cancel</span>
+                          </ToastCloseTrigger>
+                        </div>
                       </div>
-                    </div>
-                  </ToastPrimitive>
-                ))
+                    </ToastPrimitive>
+                  );
+                })
               }
             </ToastGroup>
           ))
@@ -79,3 +92,5 @@ export function ToastProvider({ children }: PropsWithChildren<{}>) {
     </ToastProviderPrimitive>
   );
 }
+
+export { useToast };
