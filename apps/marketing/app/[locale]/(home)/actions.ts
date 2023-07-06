dfee98ea8cd27a1add46cc988a997bb682e85890
Jonathan Wilke <info@jonathan-wilke.de>
2023-07-06 07:38:27 +0200

    fix: send mail for newsletter

--- apps/marketing/app/[locale]/(home)/actions.ts
+++ apps/marketing/app/[locale]/(home)/actions.ts
@@ -5,7 +5,6 @@ import { sendEmail } from "mail";
 export async function saveEmail(email: string) {
   await sendEmail({
     to: email,
-    subject: "Welcome to our newsletter",
     templateId: "newsletterSignup",
   });
 }
