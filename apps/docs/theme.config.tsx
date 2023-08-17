9b37580ee4a8dbbd037700647570efdec369cb21
Jonathan Wilke <info@jonathan-wilke.de>
2023-08-17 14:10:37 +0200

    feat: add docs

---
+++ apps/docs/theme.config.tsx
@@ -0,0 +1,16 @@
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <strong>acme documentation</strong>,
+  project: {
+    link: "https://supastarter.dev",
+  },
+  chat: {
+    link: "https://discord.com",
+  },
+  footer: {
+    text: "© copyright by supastarter",
+  },
+};
+
+export default config;
