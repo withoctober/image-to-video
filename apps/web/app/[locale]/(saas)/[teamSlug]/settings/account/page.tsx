31aa895a54e84ba52f53483a8c88a453f2e2b137
Jonathan Wilke <info@jonathan-wilke.de>
2023-08-22 08:34:04 +0200

    fix: remove redirect on account settings page

--- apps/web/app/[locale]/(saas)/[teamSlug]/settings/account/page.tsx
+++ apps/web/app/[locale]/(saas)/[teamSlug]/settings/account/page.tsx
@@ -1,6 +1,6 @@
 import { ChangeNameForm, ChangePasswordForm } from "@saas/settings/components";
 import { createApiCaller } from "api";
-import { getTranslator, redirect } from "next-intl/server";
+import { getTranslator } from "next-intl/server";
 
 export async function generateMetadata({ params: { locale } }) {
   const t = await getTranslator(locale, "settings.account");
@@ -13,10 +13,6 @@ export async function generateMetadata({ params: { locale } }) {
 export default async function AccountSettingsPage() {
   const apiCaller = await createApiCaller();
   const user = await apiCaller.user.info();
-
-  if (!user) {
-    redirect("/signin");
-  }
 
   return (
     <div className="grid gap-6">
