1d7b9bc8b7001d9e1bbf5ab526d6c3359d11af90
Jonathan Wilke <info@jonathan-wilke.de>
2023-09-07 14:24:20 +0200

    refactor: remove docs app (temp)

--- apps/docs/tailwind.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  presets: [require("tailwind-config")],
-  content: ["./app/**/*.tsx", "./components/**/*.tsx"],
-  plugins: [require("tailwindcss-animate")],
-};
--- apps/web/app/[locale]/(saas)/[teamSlug]/settings/layout.tsx
+++ apps/web/app/[locale]/(saas)/[teamSlug]/settings/layout.tsx
@@ -1,12 +1,25 @@
-import { PageHeader, TabGroup } from "@saas/shared/components";
-import { useTranslations } from "next-intl";
+import { appConfig } from "@config";
+import { PageHeader } from "@saas/shared/components";
+import { createApiCaller } from "api";
+import BoringAvatar from "boring-avatars";
+import Link from "next-intl/link";
+import { getTranslator } from "next-intl/server";
 import { PropsWithChildren } from "react";
 
-export default function SettingsLayout({
+export default async function SettingsLayout({
   children,
-  params: { teamSlug },
-}: PropsWithChildren<{ params: { teamSlug: string } }>) {
-  const t = useTranslations();
+  params: { teamSlug, locale },
+}: PropsWithChildren<{ params: { teamSlug: string; locale: string } }>) {
+  const t = await getTranslator(locale);
+
+  const apiCaller = await createApiCaller();
+  const user = await apiCaller.auth.user();
+
+  const activeTeam = user?.teamMemberships?.find(
+    (membership) => membership.team.slug === teamSlug,
+  )?.team;
+
+  if (!activeTeam) return null;
 
   return (
     <div className="container max-w-5xl py-8">
@@ -15,17 +28,53 @@ export default function SettingsLayout({
         subtitle={t("settings.subtitle")}
       />
 
-      <div className="mt-6">
-        <TabGroup
-          items={["account", "team", "billing"].map((segment) => ({
-            label: t(`settings.${segment}.title` as any),
-            href: `/${teamSlug}/settings/${segment}`,
-            segment,
-          }))}
-          className="mb-6"
-        />
+      <div className="align-start mt-8 flex flex-col gap-8 md:flex-row">
+        <div className="w-full md:max-w-[250px]">
+          <div className="space-y-6">
+            <div>
+              <div className="flex justify-start gap-2">
+                <BoringAvatar
+                  size={16}
+                  name={activeTeam.name}
+                  variant="marble"
+                  colors={appConfig.teams.avatarColors}
+                />
+                <h2 className="text-muted-foreground text-xs">
+                  {t("settings.menu.team.title")}
+                </h2>
+              </div>
 
-        {children}
+              <ul className="list-none">
+                <li>
+                  <Link href="/settings/team/general" className="block py-1">
+                    {t("settings.menu.team.general")}
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/settings/team/members" className="block py-1">
+                    {t("settings.menu.team.members")}
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <h2 className="text-muted-foreground text-xs">
+                {t("settings.menu.account.title")}
+              </h2>
+
+              <ul className="list-none">
+                <li>
+                  <Link href="/settings/general/general" className="block py-1">
+                    {t("settings.menu.account.general")}
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex-1">{children}</div>
       </div>
     </div>
   );
