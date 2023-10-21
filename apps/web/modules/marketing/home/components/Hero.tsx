5109f4be9fe942fb7259a427dae039e816529ad7
Jonathan Wilke <info@jonathan-wilke.de>
2023-10-21 11:55:17 +0200

    fix: hero title

--- apps/web/modules/marketing/home/components/Hero.tsx
+++ apps/web/modules/marketing/home/components/Hero.tsx
@@ -4,8 +4,8 @@ export function Hero() {
   return (
     <nav className=" border-b py-24">
       <div className="container text-center">
-        <h1 className="text-5xl font-bold lg:text-7xl">
-          Your revolutionary SaaS
+        <h1 className="mx-auto max-w-3xl text-5xl font-bold lg:text-7xl">
+          Your revolutionary Next.js SaaS
         </h1>
 
         <p className="mt-5 text-lg opacity-75">
