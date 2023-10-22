71a6e74621c9db2f0dff3004c32c14e20a00b80f
abd <ace4port@gmail.com>
2023-10-22 07:44:09 +0545

    Prevent password view input to accidental submit form

--- apps/web/modules/ui/components/PasswordInput.tsx
+++ apps/web/modules/ui/components/PasswordInput.tsx
@@ -23,6 +23,7 @@ export function PasswordInput({
         onChange={(e) => onChange(e.target.value)}
       />
       <button
+        type="button"
         onClick={() => setShowPassword(!showPassword)}
         className="text-primary absolute inset-y-0 right-0 flex items-center pr-4 text-xl"
       >
