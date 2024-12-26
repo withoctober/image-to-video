3324ddc5b015ccbc3fe742398f79e01f2ae1e63b
Jonathan Wilke <info@jonathan-wilke.de>
2024-12-26 10:10:50 +0100

    fix: seed script for admin users

--- tooling/scripts/src/create-user.ts
+++ tooling/scripts/src/create-user.ts
@@ -4,22 +4,48 @@ import { logger } from "@repo/logs";
 import { nanoid } from "nanoid";
 
 async function main() {
-	logger.log("Seeding application...");
+	logger.info("Let's create a new user for your application!");
+
+	const email = await logger.prompt("Enter an email:", {
+		required: true,
+		placeholder: "admin@example.com",
+		type: "text",
+	});
+
+	const name = await logger.prompt("Enter a name:", {
+		required: true,
+		placeholder: "Adam Admin",
+		type: "text",
+	});
+
+	const isAdmin = await logger.prompt("Should user be an admin?", {
+		required: true,
+		type: "confirm",
+		default: false,
+	});
 
 	const authContext = await auth.$context;
-	const adminEmail = "admin@example.com";
 	const adminPassword = nanoid(16);
 	const hashedPassword = await authContext.password.hash(adminPassword);
-	const adminUser = await db.user.upsert({
+
+	// check if user exists
+	const user = await db.user.findUnique({
 		where: {
-			email: adminEmail,
+			email,
 		},
-		update: {},
-		create: {
+	});
+
+	if (user) {
+		logger.error("User with this email already exists!");
+		return;
+	}
+
+	const adminUser = await db.user.create({
+		data: {
 			id: nanoid(),
-			email: adminEmail,
-			name: "Adam Admin",
-			role: "admin",
+			email,
+			name,
+			role: isAdmin ? "admin" : "user",
 			emailVerified: true,
 			createdAt: new Date(),
 			updatedAt: new Date(),
@@ -45,12 +71,8 @@ async function main() {
 		});
 	}
 
-	logger.success("Application seeded successfully!");
-	logger.info(
-		`Admin user created: ${adminUser.email} (${adminUser.id}) - PW: ${
-			adminPassword
-		}`,
-	);
+	logger.success("User created successfully!");
+	logger.info(`Here is the password for the new user: ${adminPassword}`);
 }
 
 main();
