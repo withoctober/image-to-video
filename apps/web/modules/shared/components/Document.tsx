b555a685d6db887880abd93dc4cb7a947cb1fdea
Jonathan Wilke <info@jonathan-wilke.de>
2025-01-06 17:23:46 +0100

    fix: remove unused metadata definition

--- apps/web/modules/shared/components/Document.tsx
+++ apps/web/modules/shared/components/Document.tsx
@@ -4,20 +4,11 @@ import { ApiClientProvider } from "@shared/components/ApiClientProvider";
 import { Toaster } from "@ui/components/toaster";
 import { cn } from "@ui/lib";
 import { Provider as JotaiProvider } from "jotai";
-import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
 import { Poppins } from "next/font/google";
 import NextTopLoader from "nextjs-toploader";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 import type { PropsWithChildren } from "react";
-
-export const metadata: Metadata = {
-	title: {
-		absolute: "supastarter.nextjs - Application",
-		default: "supastarter.nextjs- Application",
-		template: "%s | supastarter.nextjs - Application",
-	},
-};
 
 const sansFont = Poppins({
 	subsets: ["latin"],
