1aac612abc2e9df8d8899122b5c71a07b53bc758
Jonathan Wilke <info@jonathan-wilke.de>
2025-01-22 08:56:41 +0100

    refactor: ai package

--- packages/ai/index.ts
+++ packages/ai/index.ts
@@ -1,6 +1,8 @@
-import { openai } from "./provider/openai";
+import { openai } from "@ai-sdk/openai";
+
+export const textModel = openai("gpt-4o-mini");
+export const imageModel = openai("dall-e-3");
+export const audioModel = openai("whisper-1");
 
 export * from "ai";
 export * from "./lib";
-
-export const textModel = openai("gpt-4o-mini");
