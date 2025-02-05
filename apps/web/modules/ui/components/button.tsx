49d5c51079cccfcb7626b00fba5b78b664470ae8
Jonathan Wilke <info@jonathan-wilke.de>
2025-02-05 15:22:46 +0100

    fix: button svg

--- apps/web/modules/ui/components/button.tsx
+++ apps/web/modules/ui/components/button.tsx
@@ -7,7 +7,7 @@ import type { VariantProps } from "class-variance-authority";
 import { cva } from "class-variance-authority";
 
 const buttonVariants = cva(
-	"flex items-center justify-center border font-semibold transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:pointer-events-none disabled:opacity-50 [&>svg+svg]:hidden",
+	"flex items-center justify-center border font-semibold transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:pointer-events-none disabled:opacity-50 [&>svg]:mr-1.5 [&>svg]:opacity-60 [&>svg+svg]:hidden",
 	{
 		variants: {
 			variant: {
@@ -25,7 +25,7 @@ const buttonVariants = cva(
 				md: "h-9 rounded-md px-4 text-sm",
 				sm: "h-8 rounded-md px-3 text-xs",
 				lg: "h-11 rounded-md px-6 text-base",
-				icon: "size-9 rounded-md",
+				icon: "size-9 rounded-md [&>svg]:m-0 [&>svg]:opacity-60",
 			},
 		},
 		defaultVariants: {
