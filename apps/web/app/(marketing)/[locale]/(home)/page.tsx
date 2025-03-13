08c23c0a08e849b9373dab36f21269dcca8a52b3
withoctober <octoberdev@163.com>
2025-03-13 22:31:43 +0800

    fix some error

--- apps/web/app/(marketing)/[locale]/(home)/page.tsx
+++ apps/web/app/(marketing)/[locale]/(home)/page.tsx
@@ -1,13 +1,11 @@
+import { CallToAction } from "@marketing/home/components/CallToAction";
 import { FaqSection } from "@marketing/home/components/FaqSection";
-import { Usecases } from "@marketing/home/components/Usecases";
 import Generator from "@marketing/home/components/Generator";
 import { Hero } from "@marketing/home/components/Hero";
 import { HowItWorks } from "@marketing/home/components/HowItWorks";
-import { Newsletter } from "@marketing/home/components/Newsletter";
-import { PricingSection } from "@marketing/home/components/PricingSection";
+import { Usecases } from "@marketing/home/components/Usecases";
 import type { Metadata } from "next";
 import { getTranslations, setRequestLocale } from "next-intl/server";
-import { CallToAction } from "@marketing/home/components/CallToAction";
 
 export async function generateMetadata({
 	params,
